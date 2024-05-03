a86bc00a59903847245a3a62e86e60666e6dee60
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-05-03 08:40:19 +0100

    class

---
+++ javascript_course/firstIndex.js
@@ -0,0 +1,12 @@
+
+// let visitorN = prompt("what is your name")
+// console.log(visitorN);
+// alert(`welcome ${visitorN}`);
+
+// let hasEaten = confirm("have you eaten today");
+// console.log(hasEaten)
+
+let a =7;
+let b= ++a;
+console.log(a);
+console.log(b);
